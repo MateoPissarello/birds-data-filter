45b568864714b419772ce5a51d0c78d547876849
MateoPissarello <mateopissa@gmail.com>
2024-04-26 13:08:11 -0500

    work

--- script.py
+++ script.py
@@ -7,18 +7,17 @@ origin_route = "/home/pissarello-dev/Universidad/HPC/parcial-2/wavfiles"
 destiny_route = "/home/pissarello-dev/Universidad/HPC/parcial-2/wavfiles_filtered"
 df = pd.read_csv(csv_route)
 desired_columns = ["name", "filename"]
-filtered_df= df[desired_columns]
+filtered_df = df[desired_columns]
 
 if not os.path.exists(destiny_route):
     os.makedirs(destiny_route)
 
 for specie in filtered_df["name"].unique():
     specie_df = filtered_df[filtered_df["name"] == specie]
-    
+
     selected_files = specie_df.sample(n=100)["filename"]
 
     for file in selected_files:
         origin_file = os.path.join(origin_route, file)
         destiny_file = os.path.join(destiny_route, file)
         os.system(f"cp {origin_file} {destiny_file}")
-
